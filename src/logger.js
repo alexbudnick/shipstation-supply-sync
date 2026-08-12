f1fb060860e108bf2e8bc3e0370f0a1c10ce4a50
alexbudnick <alexbudnick@gmail.com>
2026-08-11 23:55:58 -0400

    Delete src directory

--- src/logger.js
+++
@@ -1,5 +0,0 @@
-export const logger = {
-  info(message, obj = {}) { console.log(`[INFO] ${message}`, Object.keys(obj).length ? JSON.stringify(obj, null, 2) : ''); },
-  warn(message, obj = {}) { console.warn(`[WARN] ${message}`, Object.keys(obj).length ? JSON.stringify(obj, null, 2) : ''); },
-  error(message, obj = {}) { console.error(`[ERROR] ${message}`, Object.keys(obj).length ? JSON.stringify(obj, null, 2) : ''); },
-};
--- src/test_config.js
+++
@@ -1,3 +0,0 @@
-import { assertRequiredConfig, publicConfig } from './config.js';
-assertRequiredConfig();
-console.log(JSON.stringify({ ok: true, config: publicConfig() }, null, 2));
